50051adf4830a015b3aa40d7ed308e8b254c6a41
manou141 <manou141@hotmail.com>
2023-05-21 22:56:20 -0400

    Update SystemDAO.java
    added three methods  :
        getQuestionsForQuizz(int quizId)
        rightOptionsForQuestion(int questionId)
        getUsedQueizzes()

--- src/main/java/data/models/dao/SystemDAO.java
+++ src/main/java/data/models/dao/SystemDAO.java
@@ -14,25 +14,18 @@ public class SystemDAO implements ISystemDAO {
     }
 
     private List<Question> getQuestionsByDifficulty(String difficulty) {
-        Query query = manager.createQuery("SELECT q FROM Question q WHERE q.difficulte =:dif");
-        query.setParameter("dif", difficulty);
+        Query query = manager.createQuery("SELECT q FROM Question q WHERE q.difficulte = :difficulty");
+        query.setParameter("difficulty", difficulty);
         List<Question> questions = query.getResultList();
         return questions;
     }
 
-    private List<Question> getRandomQuestions(List<Question> list, int N)
-    {
+    private List<Question> getRandomQuestions(List<Question> list, int N) {
         Random rand = new Random();
-        // create a temporary list for storing
-        // selected element
         List<Question> newList = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            // take a random index between 0 to size
-            // of given List
             int randomIndex = rand.nextInt(list.size());
-            // add element in temporary list
             newList.add(list.get(randomIndex));
-            // Remove selected element from original list
             list.remove(randomIndex);
         }
         return newList;
@@ -40,29 +33,41 @@ public class SystemDAO implements ISystemDAO {
 
     @Override
     public Quiz addNewQuiz(String title) {
+        EntityTransaction transaction = manager.getTransaction();
+        transaction.begin();
+        try {
+            Quiz quiz = new Quiz();
+            quiz.setTitre(title);
+            manager.persist(quiz);
+            transaction.commit();
+            return quiz;
+        } catch (Exception e) {
+            transaction.rollback();
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     @Override
-    public List<Question> addRandomQuestionsForQuiz(int QuizID, int N, String difficulty) {
+    public List<Question> addRandomQuestionsForQuiz(int quizId, int N, String difficulty) {
         EntityTransaction transaction = manager.getTransaction();
         transaction.begin();
         try {
-            Quiz quiz = manager.find(Quiz.class, QuizID);
+            Quiz quiz = manager.find(Quiz.class, quizId);
             if (quiz != null) {
                 List<Question> questions = getQuestionsByDifficulty(difficulty);
-                if(N > questions.size())
+                if (N > questions.size()) {
                     N = questions.size();
+                }
                 List<Question> randomQuestions = getRandomQuestions(questions, N);
                 for (Question question : randomQuestions) {
-                    QuizQuestionPK quizQuestionPK = new QuizQuestionPK(quiz.getQuizId(),question.getQuestionId());
+                    QuizQuestionPK quizQuestionPK = new QuizQuestionPK(quizId, question.getQuestionId());
                     QuizQuestion quizQuestion = new QuizQuestion(quizQuestionPK, 0, quiz, question);
                     manager.persist(quizQuestion);
                 }
                 transaction.commit();
                 return randomQuestions;
             }
-
         } catch (Exception e) {
             transaction.rollback();
             System.out.println(e.getMessage());
@@ -72,31 +77,65 @@ public class SystemDAO implements ISystemDAO {
 
     @Override
     public List<Question> getQuestionsForQuiz(int quizId) {
-        return null;
+        EntityTransaction transaction = EntityManagerSingleton.getInstance().manager.getTransaction();
+        List<Question> questions = new ArrayList<>();
+
+        try {
+            transaction.begin();
+            Query query = EntityManagerSingleton.getInstance().manager.createQuery("SELECT qq.questionByQuestionId FROM QuizQuestion qq WHERE qq.quizByQuizId.quizId = :quizId");
+            query.setParameter("quizId", quizId);
+            questions = query.getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            System.out.println(e.getMessage());
+        }
+        return questions;
     }
 
     @Override
     public List<Options> optionsForQuestion(int questionId) {
-
-        Query query = manager.createQuery("SELECT o FROM Options o WHERE o.questionId =:qi");
-        query.setParameter("qi",  questionId);
+        Query query = manager.createQuery("SELECT o FROM Options o WHERE o.questionId = :questionId");
+        query.setParameter("questionId", questionId);
         List<Options> options = query.getResultList();
-
         return options;
-
     }
 
     @Override
     public List<Quiz> getNotUsedQuizzes() {
-        return null;
+        Query query = manager.createQuery("SELECT q FROM Quiz q WHERE NOT EXISTS (SELECT qq FROM QuizQuestion qq WHERE qq.quizByQuizId = q)");
+        List<Quiz> quizzes = query.getResultList();
+        return quizzes;
     }
+
+    @Override
+    public List<Quiz> getUsedQuizzes() {
+        EntityTransaction transaction = EntityManagerSingleton.getInstance().manager.getTransaction();
+        List<Quiz> quizzes = new ArrayList<>();
+
+        try {
+            transaction.begin();
+            Query query = EntityManagerSingleton.getInstance().manager.createQuery("SELECT q FROM Quiz q WHERE EXISTS (SELECT qq FROM QuizQuestion qq WHERE qq.quizByQuizId = q)");
+            quizzes = query.getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            System.out.println(e.getMessage());
+        }
+        return quizzes;
+    }
+
 
     @Override
     public QuizQuestion updateQuizQuestion(int quizId, int questionId, int selectedOptionId) {
         EntityTransaction transaction = manager.getTransaction();
         transaction.begin();
         try {
-            QuizQuestionPK quizQuestionPK = new QuizQuestionPK(quizId,questionId);
+            QuizQuestionPK quizQuestionPK = new QuizQuestionPK(quizId, questionId);
             QuizQuestion quizQuestion = manager.find(QuizQuestion.class, quizQuestionPK);
             if (quizQuestion != null) {
                 quizQuestion.setSelectedOptionID(selectedOptionId);
@@ -112,16 +151,32 @@ public class SystemDAO implements ISystemDAO {
 
     @Override
     public Options rightOptionsForQuestion(int questionId) {
-        return null;
+        EntityTransaction transaction = EntityManagerSingleton.getInstance().manager.getTransaction();
+        Options correctOption = null;
+
+        try {
+            transaction.begin();
+            Query query = EntityManagerSingleton.getInstance().manager.createQuery("SELECT o FROM Options o WHERE o.questionId = :questionId AND o.estVrai = true");
+            query.setParameter("questionId", questionId);
+            List<Options> options = query.getResultList();
+            if (!options.isEmpty()) {
+                correctOption = options.get(0);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            System.out.println(e.getMessage());
+        }
+        return correctOption;
     }
 
-    @Override
-    public List<Quiz> getUsedQuizzes() {
-        return null;
-    }
 
     @Override
     public QuizQuestion getQuizQuestion(int quizId, int questionId) {
-        return null;
+        QuizQuestionPK quizQuestionPK = new QuizQuestionPK(quizId, questionId);
+        QuizQuestion quizQuestion = manager.find(QuizQuestion.class, quizQuestionPK);
+        return quizQuestion;
     }
 }
